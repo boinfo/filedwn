ca1f85fd2326fff47aec63fd1fde6264677ef728
boinfo <142826699+boinfo@users.noreply.github.com>
2024-02-26 14:29:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,35 @@
 const express = require('express');
-const { downloadTrack } = require('@nechlophomeriaa/spotifydl');
-
-const app = express();
-app.set("json spaces", 4);
-const PORT = 3000;
-const fs = require('fs');
-let axios = require("axios");
+const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', async function (req, res) {
-res.json({status:"ok"});
+app.get('/', (req, res) => {
+    res.json({ status: "ok" });
 });
-
 
 app.get('/audio', async (req, res) => {
-  const url = req.query.url;
+    const url = req.query.url;
 
-  if (!url) {
-    return res.status(400).json({ error: 'Invalid URL' });
-  }
+    if (!url) {
+        return res.status(400).json({ error: 'Invalid URL' });
+    }
 
-  try {
-    const response = await axios.get(url, { responseType: 'arraybuffer' });
-    const fileContent = response.data;
+    try {
+        const response = await axios.get(url, { responseType: 'stream' });
+        const randomFileName = uuidv4() + '-by__ytmp3.pub.mp3';
 
-    const randomFileName = uuidv4() + '-by__ytmp3.pub.mp3';
-    const filePath = '/tmp/' + randomFileName; // or any desired directory
+        res.setHeader('Content-Type', 'application/octet-stream');
+        res.setHeader('Content-Disposition', 'attachment; filename="' + randomFileName + '"');
 
-    fs.writeFileSync(filePath, fileContent);
-
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename="' + randomFileName + '"');
-    res.download(filePath, randomFileName, (err) => {
-      if (err) {
-        console.error('Error downloading file:', err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-
-      // Clean up the temporary file after download
-      fs.unlinkSync(filePath);
-    });
-  } catch (error) {
-    console.error('Error fetching file:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+        response.data.pipe(res);
+    } catch (error) {
+        console.error('Error fetching file:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
