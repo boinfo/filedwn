1b50caa7c4177dd84c78982f9b467fa7dbb4344f
boinfo <142826699+boinfo@users.noreply.github.com>
2024-02-27 09:44:20 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const express = require('express');
-const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
-
+const axios = require('axios');
+const admin = require('firebase-admin');
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({ status: "ok" });
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./serviceAccountKey.json'); // Assuming the JSON key file is in the current folder
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'audiofiles-82ffa.appspot.com', // Your Firebase Storage bucket URL
 });
+
+const bucket = admin.storage().bucket();
 
 app.get('/audio', async (req, res) => {
-    const url = req.query.url;
+  const url = req.query.url;
 
-    if (!url) {
-        return res.status(400).json({ error: 'Invalid URL' });
-    }
+  if (!url) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
 
-    try {
-        const response = await axios.get(url, { responseType: 'stream' });
-        const randomFileName = uuidv4() + '-by__ytmp3.pub.mp3';
+  try {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const fileContent = response.data;
 
-        res.setHeader('Content-Type', 'application/octet-stream');
-        res.setHeader('Content-Disposition', 'attachment; filename="' + randomFileName + '"');
+    const randomFileName = uuidv4() + '-by__ytmp3.pub.mp3';
 
-        response.data.pipe(res);
-    } catch (error) {
-        console.error('Error fetching file:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    // Upload file to Firebase Storage
+    await bucket.file(randomFileName).save(fileContent, {
+      contentType: 'audio/mpeg', // Adjust content type as needed
+    });
+
+    // Get download URL
+    const downloadURL = `https://storage.googleapis.com/audiofiles-82ffa.appspot.com/${randomFileName}`;
+
+    res.status(200).json({ downloadURL });
+  } catch (error) {
+    console.error('Error fetching or uploading file:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
